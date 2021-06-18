dd98272d13e65350065b2fa611802c938e1142e6
daniel <daniel@aiteam.pl>
2021-06-18 22:32:14 +1000

    task controller

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Task } from './task.model';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private taskService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.taskService.getAllTasks();
+  }
+
+  @Post()
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    console.log('title: ', title);
+    console.log('description: ', description);
+    return this.taskService.createTasks(title, description);
+  }
+}
